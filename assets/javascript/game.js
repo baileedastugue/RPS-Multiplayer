d90d3f0f65e030815ac4e522718a97fe9bb2d1bf
Bailee Dastugue <bailee.dast@gmail.com>
2020-03-11 13:12:54 -0500

    determines winner, stores num games played + number of ties, stores individual player wins

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,6 +11,7 @@ var playerTwo = {
 var pOneMove = false;
 var pTwoMove = false;
 var numGames = 0;
+var numTies = 0;
 var recentMove = "";
 
 $(document).keyup(function (event) {
@@ -25,25 +26,32 @@ $(document).keyup(function (event) {
             playerTwo.recentPlay = recentMove;
             console.log(playerTwo.recentPlay);
             pTwoMove = true;
-        }
-        else {
-            console.log("yeehaw");
+            determineResults();
         }
     }
     else {
         console.log("please press the letter 'r' for rock, 'p' for paper, or 's' for scissors!");
-    }   
+    }
 });
 
 
-// function makeMoves () {
-//     return recentMove;
-// }
-// console.log(makeMoves());
-
-
-// function storeMoves () {
-//     if (!pOneMove) {
-
-//     }
-// }
+function determineResults () {
+    if (playerOne.recentPlay === "r" && playerTwo.recentPlay === "p" ||
+        playerOne.recentPlay === "p" && playerTwo.recentPlay === "r" || 
+        playerOne.recentPlay === "s" && playerTwo.recentPlay === "p") {
+            console.log("player one wins");
+            playerOne.numWins++;
+        }
+    else if (playerOne.recentPlay === playerTwo.recentPlay) {
+        console.log("tie game");
+        numTies++;
+    }
+    else {
+        console.log("player two wins");
+        playerTwo.numWins++;
+    }
+    numGames++;
+    console.log(numGames, numTies, playerOne.numWins, playerTwo.numWins);
+    pOneMove = false;
+    pTwoMove = false;
+}
