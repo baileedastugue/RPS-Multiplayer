b5b22576e476e9bdb7b4556ca9c4ee05340d9491
Bailee Dastugue <bailee.dast@gmail.com>
2020-03-11 12:56:02 -0500

    stores two RPS inputs in their respective object, does not allow a third input during round

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,11 +9,30 @@ var playerTwo = {
 }
 
 var pOneMove = false;
+var pTwoMove = false;
 var numGames = 0;
 var recentMove = "";
 
 $(document).keyup(function (event) {
-    recentMove = event.key;   
+    recentMove = event.key;
+    if (recentMove === "r" || recentMove === "p" || recentMove === "s") {
+        if (!pOneMove) {
+            playerOne.recentPlay = recentMove;
+            console.log(playerOne.recentPlay);
+            pOneMove = true;
+        }
+        else if (!pTwoMove) {
+            playerTwo.recentPlay = recentMove;
+            console.log(playerTwo.recentPlay);
+            pTwoMove = true;
+        }
+        else {
+            console.log("yeehaw");
+        }
+    }
+    else {
+        console.log("please press the letter 'r' for rock, 'p' for paper, or 's' for scissors!");
+    }   
 });
 
 
