d98f3a0f6cd51c763f92b3b8e3b2a1f0e669e16b
Bailee Dastugue <bailee.dast@gmail.com>
2020-03-11 13:44:52 -0500

    firebase initiated, pushes game results and player moves to database

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,19 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyC7TtrAdMjN6XBW-TQPLcqKU5ZAAxLiJz0",
+    authDomain: "rps-multi-95941.firebaseapp.com",
+    databaseURL: "https://rps-multi-95941.firebaseio.com",
+    projectId: "rps-multi-95941",
+    storageBucket: "rps-multi-95941.appspot.com",
+    messagingSenderId: "344114657840",
+    appId: "1:344114657840:web:e0d3f07389b7a0150f0bf5"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
 var playerOne = {
     recentPlay: "",
     numWins: 0
@@ -13,6 +29,13 @@ var pTwoMove = false;
 var numGames = 0;
 var numTies = 0;
 var recentMove = "";
+var results = "";
+
+var recentGame = {
+    playerOneMove: playerOne.recentPlay,
+    playerTwoMove: playerTwo.recentPlay,
+    gameResults: results
+}
 
 $(document).keyup(function (event) {
     recentMove = event.key;
@@ -39,19 +62,27 @@ function determineResults () {
     if (playerOne.recentPlay === "r" && playerTwo.recentPlay === "p" ||
         playerOne.recentPlay === "p" && playerTwo.recentPlay === "r" || 
         playerOne.recentPlay === "s" && playerTwo.recentPlay === "p") {
-            console.log("player one wins");
+            results = "player one wins";
             playerOne.numWins++;
         }
     else if (playerOne.recentPlay === playerTwo.recentPlay) {
-        console.log("tie game");
+        results = "tie";
         numTies++;
     }
     else {
-        console.log("player two wins");
+        results = "player two wins";
         playerTwo.numWins++;
     }
     numGames++;
     console.log(numGames, numTies, playerOne.numWins, playerTwo.numWins);
     pOneMove = false;
     pTwoMove = false;
+
+    recentGame = {
+        playerOneMove: playerOne.recentPlay,
+        playerTwoMove: playerTwo.recentPlay,
+        gameResults: results
+    }
+
+    database.ref().push(recentGame);
 }
